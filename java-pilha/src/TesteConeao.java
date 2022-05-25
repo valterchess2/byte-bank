dcdea625b3708aa0ce06175fb51e10d49fc76c5f
Valter Silva <valter.silva.dev@gmail.com>
2022-05-25 17:35:47 -0300

    finalizando curso exceptions

--- java-pilha/src/TesteConeao.java
+++ java-pilha/src/TesteConeao.java
@@ -1,5 +1,23 @@
 public class TesteConeao {
     public static void main(String[] args) {
 
+        try (Conexao conexao = new Conexao()) {
+            conexao.leDados();
+        } catch (IllegalStateException ex) {
+            System.out.println("Erro na Conexão");
+        }
+
+// --------------------------------------------------
+
+//        Conexao con = null;
+//        try {
+//            con = new Conexao();
+//            con.leDados();
+//        } catch (IllegalStateException ex) {
+//            System.out.println("Erro na Conexão");
+//        } finally {
+//            System.out.println("finally");
+//            if (con != null) con.fecha();
+//        }
     }
 }
